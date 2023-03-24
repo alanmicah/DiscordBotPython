04a3f524aef8a11fa6b40e10c8b3a261e34a7949
alanmicah <31675641+alanmicah@users.noreply.github.com>
2023-03-24 09:09:57 +0000

    decorator implementation

--- bot.py
+++ bot.py
@@ -8,35 +8,26 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 # GUILD = os.getenv('DISCORD_GUILD')
 
-# decorator version
-class CustomClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
+client = discord.Client()
 
-# Client an instance of CustomClient
-# which has an overridden on_ready() function
-client = CustomClient()
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
 client.run(TOKEN)
 
-# client = discord.Client(intents=discord.Intents.default())
-# # on_ready() will be called once client is ready for further action
-# @client.event
-# async def on_ready():
+# class CustomClient(discord.Client):
+#     async def on_ready(self):
+#         print(f'{self.user} has connected to Discord!')
 
-#     # discord.utils.find() can improve the simplicity and readability of this code
-#     # by replacing the for loop with an abstracted function
-#     # for guild in client.guilds:
-#     #     if guild.name == GUILD:
-#     #         break
-#     # guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-#     # get() takes the iterable and some keyword arguments, in this case name=GUILD
-#     guild = discord.utils.get(client.guilds, name=GUILD)
-#     print(
-#         f'{client.user} is connected to the following guild:\n'
-#         f'{guild.name}(id: {guild.id})'
-#     )
-
-#     members = '\n - '.join([member.name for member in guild.members])
-#     print(f'Guild Members:\n - {members}')
-
+# # Client an instance of CustomClient
+# # which has an overridden on_ready() function
+# client = CustomClient()
 # client.run(TOKEN)
